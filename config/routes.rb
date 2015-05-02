0e95b54a7fd6dad5aac1d541624eaa4a3f9c7aa6
Jason <jasonebueng@gmail.com>
2015-05-02 17:02:27 +0000

    Update Posts routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root 'welcome#index'
+  root 'welcome#testing'
   match '/whatever' => 'welcome#testing', via: [:get]
   
-  resources :posts, only: [:index, :show]
+  resources :posts, only: [:index, :show, :new, :create]
 
 end
